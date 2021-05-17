7e3a4196d2423b76b72a8c2271158e60f8f1e105
ramarajudasari <47561651+ramarajudasari@users.noreply.github.com>
2021-05-17 15:29:58 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,20 @@
+class Solution:
+	def setKthBit(self, N, K):
+	    ans=0
+	    ans=N|(1<<K)
+	    return ans
+		# code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N,K = input().split()
+		N = int(N)
+		K = int(K)
+		ob = Solution()
+		ans = ob.setKthBit(N,K)
+		print(ans)
