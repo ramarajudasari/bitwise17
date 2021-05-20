8ad801dd89c564506c648b13e62b9b069a4f6da9
ramarajudasari <47561651+ramarajudasari@users.noreply.github.com>
2021-05-20 15:33:12 +0530

    Create balancebrackets.py

---
+++ balancebrackets.py
@@ -0,0 +1,18 @@
+open_list = ["[","{","("]
+close_list = ["]","}",")"]
+def isBalanced(s):
+    stack = []
+    for i in s:
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[len(stack)-1])):
+                stack.pop()
+            else:
+                return "NO"
+    if len(stack) == 0:
+        return "YES"
+    else:
+        return "NO"
